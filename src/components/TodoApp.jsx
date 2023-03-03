214444d905ce25c2d6828200b3dd09b1fb5605c5
dazehinn <dazehinn.yahoo.fr>
2023-03-03 22:19:04 +0100

    Source file

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,11 @@
+import Header from '@/components/Header.jsx';
+import TodosLogic from '@/components/TodosLogic.jsx';
+const TodoApp = () => {
+  return (
+    <div>
+      <Header />
+      <TodosLogic />
+    </div>
+  );
+};
+export default TodoApp;
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,16 @@
+const TodoItem = ({itemProp, handleChange, delTodo}) => {
+
+  return (
+    <li>
+      <input type="checkbox" 
+        checked={itemProp.completed}
+        onChange={() => handleChange(itemProp.id)}
+      />
+
+      {itemProp.title}
+      <button onClick={() => delTodo(itemProp.id)}>Delete</button>
+      </li>
+  )
+}
+
+export default TodoItem;
