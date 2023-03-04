8542ae8f76dc384f94ee7425283f1b01b5e0c108
dazehinn <dazehinn.yahoo.fr>
2023-03-04 03:09:51 +0100

    Add entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
 
-import TodoApp from "@/components/TodoApp";
+import ReactDOM from 'react-dom/client';
 
-const domContainer = document.getElementById("root");
+import TodoApp from './components/TodoApp';
+
+import './styles/App.css';
+
+const domContainer = document.getElementById('root');
 const root = ReactDOM.createRoot(domContainer);
 root.render(
-<React.StrictMode>
-<TodoApp />
-</React.StrictMode>);
+  <React.StrictMode>
+    <TodoApp />
+  </React.StrictMode>,
+);
