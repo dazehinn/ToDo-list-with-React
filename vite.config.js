4f3e2ffc9048d9e4c476ceb2ef124129991af21d
dazehinn <dazehinn.yahoo.fr>
2023-03-04 03:11:28 +0100

    Environment file

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// ...
+/* eslint-disable */
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [react()],
+});
