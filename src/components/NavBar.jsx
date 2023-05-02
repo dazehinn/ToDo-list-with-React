1f54bf931e945d9db44e01e4f527d93930143ca3
dazehinn <dazehinn@yahoo.fr>
2023-05-02 22:47:36 +0100

    Update navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,46 +1,27 @@
-import { useEffect, useRef, useState } from 'react';
+/* eslint-disable */
+import { NavLink } from 'react-router-dom';
 
+const links = [
+  { path: '/', text: 'Home' },
+  { path: 'about', text: 'About' },
+
+];
 const NavBar = () => {
-    const [dropdown, setDropdown] = useState(false);
-    // useEffect(() => {
-    //     console.log(dropdown);
-        
-    //     document.title = `current dropdown state is ${dropdown}`;
-    // }, [dropdown] )
-    const ref = useRef();
+  <nav className="navbar">
+    <ul>
+      {
+      links.map((link) => <li className="linkk" key={link.text}>
+        <NavLink to={link.path}>
+            {link.text}
+        </NavLink>
+        </li>,
 
-    useEffect(() => {
-        const handler = (event) => {
-            if(dropdown && ref.current && ref.current.contains(event.target)) {
-                setDropdown(false);
-            }
-        };
-        document.addEventListener('mousedown',handler);
-        return () => {
-            // Cleanup the event listener
-            document.removeEventListener("mousedown", handler);
-          };
-    }, [dropdown])
-    console.log(ref);
-    return (
-        <nav>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li ref = {ref}>
-                  <button onClick={() => setDropdown((prev) => !prev) }>
-                    Services <span>&#8595;</span>
-                  </button>
-                  { dropdown && (
-                  <ul>
-                    <li>Design</li>
-                    <li>Development</li>
-                  </ul>
-)}
-                </li>
-            </ul>
-        </nav>
-    );
+      )
+
+    }
+     ,
+    </ul>
+  </nav>;
 };
 
 export default NavBar;
