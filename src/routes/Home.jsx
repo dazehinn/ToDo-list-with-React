fd40c5558a401778800ccc224d7667e2a4f1ed67
dazehinn <dazehinn@yahoo.fr>
2023-05-02 22:49:46 +0100

    Add routes components

---
+++ src/routes/Home.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import Header from './components/.Header';
+import TodosLogic from '@/components/TodosLogic.jsx';
+
+const Home = () => {
+  <div className="todos">
+    <Header />
+    <TodosLogic />
+  </div>;
+};
+
+export default Home;
